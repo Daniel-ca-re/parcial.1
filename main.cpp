cfe2b3e7ad56108e3054d6833780adf6427481ae
Daniel-ca-re <70850709+Daniel-ca-re@users.noreply.github.com>
2020-10-17 15:48:39 -0500

    0.1 plateamineto 1 y ingreso de variables

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <ctime>
+#include<conio.h>
+#include<stdio.h>
+#include<math.h>
+
+
+using namespace std;
+
+int main()
+{
+    float d;
+    float h;
+    float h2;
+    float angulo;
+    float a=9.81;
+    cout <<"ingrese la distancia horizontal que hay entre ambos cañones(en metros)"<<endl;
+    cin >> d;
+    cout <<"ingrese la distancia vertical que hay el cañon atacante y el mar(en metros)"<<endl;
+    cin >> h;
+    cout <<"ingrese la distancia vertical que hay el cañon defensor y el mar(en metros)"<<endl;
+    cin >>h2;
+    h-=h2;
+    cout <<"ingrese el angulo que se unsara en el cañon atacante"<<endl;
+    cin >>angulo;
+
+
+}
+
