8a45cb276c0bec3d36f8c407de6c1e68fca87caa
Daniel Cano <daniel.canor1@udea.edu.co>
2020-10-20 10:51:06 -0500

    1.2 se realiza exitosamente el punto 3 del parcial en donde se implemente una una busqueda a fuerza bruta iterando poco a poco en las variables

--- main.cpp
+++ main.cpp
@@ -118,12 +118,39 @@ void disparo_acertadoen_en_la_bala_atacante()
     cin >>angD;
     cout <<"ingrese el angulo que se unsara en el cañon atacante(en radianes)"<<endl;
     cin >>angO;
+    //se averigua la velocidad y el tiempo necesario para intervceptar el cañon defensor
     float t_lim =sqrt((d*tan(angO)+h)/(a/2));
     float VO=d/(cos(angO)*t_lim);
     t_lim -=2.5;
+
     if(t_lim>0)
     {
+        //imprementacion de 2 for que por medio de busqueda
+        //por fuerza bruta encuentran la velocidad necesaria para interceptar la bala
 
+
+        float VD=0;
+        for(char se_encontro='0';VD<10000;VD+=0.1)
+        {
+            for(float t=0;t<t_lim;t+=0.01)
+            {
+                float xD=VD*cos(angD)*t;
+                float yD=VD*sin(angD)*t-4.905*pow(t,2);
+                float xO=-VO*cos(angO)*(t+2.5)+d;
+                float yO=VO*sin(angO)*(t+2.5)-4.905*pow(t+2.5,2)+h;
+                if (sqrt(pow(xD-xO,2)+pow(yO-yD,2))<.025*d)
+                {
+                    cout <<"la rapidez necesaria para que el canhonaso defensor intersepte al canhonado ofensor es= " << VD <<endl;
+                    se_encontro='1';
+                    break;
+                }
+
+            }
+            if(se_encontro=='1')
+            {
+                break;
+            }
+        }
     }
     else
     {
@@ -132,5 +159,6 @@ void disparo_acertadoen_en_la_bala_atacante()
 
 
 }
+
 
 
